cf8b078dce3afdab3c47815edf1e249bef6ab08d
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-01-21 14:33:40 -0800

    add test data for a dummy post

---
+++ src/data/testPostData.js
@@ -0,0 +1,24 @@
+const post = {
+  userName: "Ricardo Camacho",
+  postTitle: "Got A New Ninja 400",
+  postDescription:
+    "Just bought my new motorcycle, it's a ninja 400, Kawasaki with ABS. Only 5 miles, still not broken in",
+  likedBy: ["My Mother", "Robert", "Joan Artiga"],
+  comments: [
+    {
+      userName: "Robert",
+      comment: "dude that's wicked",
+    },
+    {
+      userName: "My Mother",
+      comment: "are you crazy!! why aren't you answering my calls",
+    },
+    {
+      userName: "Joan Artiga",
+      comment:
+        "ricardo why aren't you answering my text messages I thought we had something special",
+    },
+  ],
+};
+
+export default post;
