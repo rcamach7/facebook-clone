69e1faa476eab188213d6d4b76c68459c39ed025
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-01-21 15:46:32 -0800

    first section (1 of 4) of post details complete - aesthetically

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
+
 export default function Post() {
   return (
     <div className="Post">
-      <p>Hello Post</p>
+      <div className="postInfoContainer">
+        <PostInfo />
+      </div>
+      <div className="postNavbarContainer"></div>
+      <div className="postCommentsContainer"></div>
+      <div className="postAddCommentContainer"></div>
+    </div>
+  );
+}
+
+function PostInfo() {
+  return (
+    <div className="PostInfo">
+      <div className="postInfo-user">
+        <FontAwesomeIcon icon="user" size="1x" />
+      </div>
+      <div className="postInfo-details">
+        <p className="details-username">Ricardo Camacho</p>
+        <p>12 hrs ago</p>
+      </div>
+      <div className="postInfo-editPost">
+        <button className="editPost-btn">
+          <FontAwesomeIcon icon={faEllipsisH} />
+        </button>
+      </div>
     </div>
   );
 }
