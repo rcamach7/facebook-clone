63dfe5dcab0f30fdab06e0330a59683fbe99f451
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-04-27 23:18:47 -0700

    create profileOverview component for profile page, showing user image, name, and navbar, and making it responsive for all viewports

---
+++ client/src/components/Profile/ProfileOverview.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCirclePlus,
+  faCamera,
+  faEllipsisH,
+} from "@fortawesome/free-solid-svg-icons";
+
+export default function ProfileOverview({ user }) {
+  return (
+    <div className="ProfileOverview">
+      <section className="image-backdrop">
+        <div className="imageContainer">
+          <img src={user ? user.profilePicture : null} alt="user" />
+        </div>
+      </section>
+      <section className="user-backdrop">
+        <p className="user-fullName">{user ? user.fullName : null}</p>
+        <div className="editProfileButtons">
+          <button className="addToStory">
+            <FontAwesomeIcon icon={faCirclePlus} /> Add New Post
+          </button>
+          <button className="editProfile">
+            <FontAwesomeIcon icon={faCamera} /> Edit Image
+          </button>
+        </div>
+
+        <ul className="profileSections">
+          <li>Friends</li>
+          <li>Messages</li>
+          <li>Notifications</li>
+          <li className="ellipse">
+            <FontAwesomeIcon icon={faEllipsisH} />
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
--- client/src/routes/Profile.jsx
+++ client/src/routes/Profile.jsx
@@ -1,6 +1,13 @@
 import Navbar from "../components/Navbar";
 import { useContext } from "react";
 import { UserContext } from "../RouteSwitch";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCirclePlus,
+  faCamera,
+  faEllipsisH,
+} from "@fortawesome/free-solid-svg-icons";
+import ProfileOverview from "../components/Profile/ProfileOverview";
 
 const Profile = () => {
   const { user } = useContext(UserContext);
@@ -10,20 +17,8 @@ const Profile = () => {
       <header>
         <Navbar icon={user ? user.profilePicture : null} />
       </header>
-      <div className="profile-main">
-        <img src={user ? user.profilePicture : null} alt="" />
-
-        <div className="profile-userInfo">
-          <h1>{user ? user.fullName : null}</h1>
-          <p>
-            <strong>UserName: </strong>
-            {user ? user.username : null}
-          </p>
-        </div>
-
-        <button className="profile-signOut">Sign Out</button>
-        <button className="profile-EditInfo">Edit Account Info</button>
-      </div>
+      {/* First major section of page - showcasing user image, name, and a sub navbar */}
+      <ProfileOverview user={user} />
     </div>
   );
 };
